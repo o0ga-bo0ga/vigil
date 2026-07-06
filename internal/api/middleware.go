d9bf2caf20a96e1f3cfc091095ec907d1499912f
o0ga-bo0ga <prakharbansal2802@gmail.com>
2026-07-06 16:53:06 +0530

    chore: update files

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,1 +1,24 @@
 package api
+
+import (
+	"net/http"
+	"os"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		xApiKey := r.Header.Get("x-api-key")
+
+		apiKey := os.Getenv("API_KEY")
+
+		if xApiKey == "" {
+			http.Error(w, "Missing api key", http.StatusUnauthorized)
+			return
+		}
+		if xApiKey != apiKey {
+			http.Error(w, "Invalid api key", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
